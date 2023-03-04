3c2839996a6d3af4b28d46020af8cc349931468f
Jakub Trnčák <trncak@rtsoft.cz>
2023-03-04 14:32:45 +0100

    Complete Home page, with responsive for mobiles

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './components/pages/home/Home';
+import About from './components/pages/about/About';
+import Handicaps from './components/pages/handicaps/Handicaps';
+import Contact from './components/pages/contact/Contact';
+import Story from './components/pages/story/Story';
+import Illness from './components/pages/illness/Illness';
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/handicaps' element={<Handicaps />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/story' element={<Story />} />
+        <Route path='/illness' element={<Illness />} />
     </Routes>
     </div>
   )
--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -1,19 +1,42 @@
 import './navigation.css';
 import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import Icon from '../icon/Icon';
 
 function Navigation() {
+
+    const [menuIsClosed, setMenuIsClosed] = useState(true);
+
+    const toggleMenu = () => {
+        setMenuIsClosed(!menuIsClosed);
+      };
+
     return (
+
+        <>
         <div className='Navigation'>
             <Link className='logo-link' to='/'><img className='logo' src="/images/logo.png" alt="logo" /></Link>
             <div className='links'>
                 <ul>
                     <li className='show activeline'><Link to='/'>Domů </Link></li>
-                    <li className='show activeline'><Link to='/'>O nás </Link></li>
-                    <li className='show activeline'><Link to='/'>Handicapy </Link></li>
-                    <li className='show activeline'><Link to='/'>Kontakt </Link></li>
+                    <li className='show activeline'><Link to='/about'>O nás </Link></li>
+                    <li className='show activeline'><Link to='/handicaps'>Handicapy </Link></li>
+                    <li className='show activeline'><Link to='/contact'>Kontakt </Link></li>
+
+                    <li className={`hidden ${menuIsClosed ? '' : 'closed'}`}><button className='menu-button' onClick={toggleMenu}><Icon name='bars' type='fas' /></button></li>
                 </ul>
             </div>
         </div>
+        <div className={`menu ${menuIsClosed ? 'closed' : ''}`}>
+            <button className='close-button' onClick={toggleMenu}><Icon name='xmark' type='fas' /></button>
+            <ul>
+                    <li><Link to='/'><Icon name='artstation' type='fab' /> Domů </Link></li>
+                    <li><Link to='/about'><Icon name='terminal' type='fas' />O nás </Link></li>
+                    <li><Link to='/handicaps'><Icon name='signal' type='fas' />Handicapy </Link></li>
+                    <li><Link to='/contact'><Icon name='signal' type='fas' />Kontakt </Link></li>
+            </ul>
+        </div>
+        </>
     )
 }
 
