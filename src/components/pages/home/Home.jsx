2cec6088f2cefb1554f14b2bf6f8f5685d44744c
Jakub Trnčák <trncak@rtsoft.cz>
2023-03-04 11:00:36 +0100

    Complete navigation

---
+++ src/components/pages/home/Home.jsx
@@ -0,0 +1,12 @@
+import Navigation from '../../navigation/Navigation';
+import './home.css';
+
+function Home() {
+    return(
+        <div className="Home">
+            <Navigation />
+        </div>
+    )
+}
+
+export default Home;
